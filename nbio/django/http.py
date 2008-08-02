58f542a62cc2d7221349d3cfe3707c7ce32ad3b5
shaderlab <shaderlab@e77911d0-6a52-0410-bea1-fbff64990c85>
2008-08-02 19:03:55 +0000

    added CSSHttpRequest and JSON helpers
    git-svn-id: https://nbio.googlecode.com/svn/nbio_django/trunk@36 e77911d0-6a52-0410-bea1-fbff64990c85

---
+++ nbio/django/http.py
@@ -0,0 +1,27 @@
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2008 nb.io"
+__author__ = "Randy Reddig - ydnar@nb.io"
+
+
+from django.http import HttpResponse
+from django.utils import simplejson
+from nbio.csshttprequest import encode
+
+
+JSON_CONTENT_TYPE = "application/javascript+json"
+CSS_CONTENT_TYPE = "text/css"
+
+
+class JSONHttpResponse(HttpResponse):
+    def __init__(self, content):
+        super(JSONHttpResponse, self).__init__(simplejson.dumps(content), JSON_CONTENT_TYPE)
+    
+
+class CSSHttpResponse(HttpResponse):
+    def __init__(self, content):
+        super(CSSHttpResponse, self).__init__(encode(content), CSS_CONTENT_TYPE)
+
+
+class JSONCSSHttpResponse(CSSHttpResponse):
+    def __init__(self, content):
+        super(JSONCSSHttpResponse, self).__init__(simplejson.dumps(content))
