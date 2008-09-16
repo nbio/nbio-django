136c36cae443cd758f36c670e87fa875317911a1
shaderlab@gmail.com <shaderlab@gmail.com@e77911d0-6a52-0410-bea1-fbff64990c85>
2008-09-16 02:50:03 +0000

    refined nb.io Django utilities
    git-svn-id: https://nbio.googlecode.com/svn/nbio_django/trunk@44 e77911d0-6a52-0410-bea1-fbff64990c85

---
+++ nbio/django/shortcuts.py
@@ -0,0 +1,18 @@
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2008 nb.io"
+__author__ = "Randy Reddig - ydnar@nb.io"
+
+
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.template import loader
+
+
+DEFAULT_CONTENT_TYPE = 'text/html'
+
+
+def render_response(request, template, dictionary=None, content_type=DEFAULT_CONTENT_TYPE):
+    if template.__class__ == str:
+        template = loader.get_template(template)
+    c = RequestContext(request, dictionary)
+    return HttpResponse(template.render(c), content_type)
