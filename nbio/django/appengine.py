2564503e229f4f24aa3f1f176712ded39bbc3aac
Randy Reddig <ydnar@nb.io>
2008-09-18 19:11:54 +0000

    added App Engine auth middleware

---
+++ nbio/django/appengine.py
@@ -0,0 +1,22 @@
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2008 nb.io"
+__author__ = "Randy Reddig - ydnar@nb.io"
+
+
+import logging
+import re
+from django.conf import settings
+from django.http import HttpResponseForbidden
+from google.appengine.api import users
+
+
+
+class AuthMiddleware:
+    def process_request(self, request):
+        """
+        Process the request, and attempt to authenticate using Google's App Engine user API
+        """
+        email = users.get_current_user().email()
+        if email not in settings.ALLOWED_USERS:
+            return HttpResponseForbidden("User %s does not have permission to view this page." % email)
+        return None
