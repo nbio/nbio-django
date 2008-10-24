0c12f2ac3802f0b1d26f802c9d7aca85fbfd8b98
shaderlab@gmail.com <shaderlab@gmail.com@e77911d0-6a52-0410-bea1-fbff64990c85>
2008-10-24 02:12:21 +0000

    added more liberal iri_to_uri utility method that allows unquoted @ symbols in URLs
    git-svn-id: https://nbio.googlecode.com/svn/nbio_django/trunk@60 e77911d0-6a52-0410-bea1-fbff64990c85

---
+++ nbio/django/utils.py
@@ -0,0 +1,29 @@
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2008 nb.io"
+__author__ = "Randy Reddig - ydnar@nb.io"
+
+
+import urllib
+from django.utils.encoding import smart_str
+
+
+def iri_to_uri(iri):
+    """
+    Convert an Internationalized Resource Identifier (IRI) portion to a URI
+    portion that is suitable for inclusion in a URL.
+
+    This is the algorithm from section 3.1 of RFC 3987.  However, since we are
+    assuming input is either UTF-8 or unicode already, we can simplify things a
+    little from the full method.
+
+    Returns an ASCII string containing the encoded result.
+    
+    Slightly more liberal than the Django built-in
+    """
+    # The list of safe characters here is constructed from the printable ASCII
+    # characters that are not explicitly excluded by the list at the end of
+    # section 3.1 of RFC 3987.
+    # added '@' to allowed set
+    if iri is None:
+        return iri
+    return urllib.quote(smart_str(iri), safe='/#%[]=:;$&()+,!?*@')
