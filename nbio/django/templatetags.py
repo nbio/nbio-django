2e81025e59e6fb8b69dfff6206c283c6f51381a1
shaderlab@gmail.com <shaderlab@gmail.com@e77911d0-6a52-0410-bea1-fbff64990c85>
2008-10-26 02:30:46 +0000

    added app_url template tag for sitemap.xml support
    git-svn-id: https://nbio.googlecode.com/svn/nbio_django/trunk@61 e77911d0-6a52-0410-bea1-fbff64990c85

--- nbio/django/templatetags.py
+++ nbio/django/templatetags.py
@@ -8,22 +8,36 @@ from django.conf import settings
 from nbio.django.shortcuts import build_url
 
 
-class StaticUrlsNode(Node):
-    def __init__(self, path):
+class UrlNode(Node):
+    def __init__(self, host=None, path=''):
+        self.host = host
         self.path = path
+        if not self.path.startswith('/'):
+            self.path = '/' + self.path
 
     def render(self, context):
         request = context.get('request')
-        return build_url(request, host=settings.HOSTS['static'], path=self.path)
+        return build_url(request, host=self.host, path=self.path)
 
 
 def static_url(parser, token):
     try:
+        path = ''
         tag_name, path = token.split_contents()
     except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires exactly two arguments" % token.contents.split()[0]
-    return StaticUrlsNode(path)
+        pass
+    return UrlNode(host=settings.HOSTS['static'], path=path)
+
+
+def app_url(parser, token):
+    try:
+        path = ''
+        tag_name, path = token.split_contents()
+    except ValueError:
+        pass
+    return UrlNode(host=settings.HOSTS['app'], path=path)
 
 
 register = Library()
 register.tag("static_url", static_url)
+register.tag("app_url", app_url)
