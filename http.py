3efe88695dbbe6965619fa3102557559a9cc509d
Cameron Walters <cameron.walters@gmail.com>
2009-05-24 13:10:19 -0700

    Added callback support to JSONHttpResponse

--- http.py
+++ http.py
@@ -13,10 +13,12 @@ CSS_CONTENT_TYPE = "text/css"
 
 
 class JSONHttpResponse(HttpResponse):
-    def __init__(self, content):
-        super(JSONHttpResponse, self).__init__(simplejson.dumps(content), JSON_CONTENT_TYPE)
+    def __init__(self, content, callback=None):
+        json = simplejson.dumps(content)
+        if callback:
+            json = unicode(callback) + '(' + json + ')'
+        super(JSONHttpResponse, self).__init__(json, JSON_CONTENT_TYPE)
     
-
 class CSSHttpResponse(HttpResponse):
     def __init__(self, content):
         try:
